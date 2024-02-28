8af2052fd3ee71a2287e9d96150569bd61b4cbb1
songmoro <wlqgkr@naver.com>
2024-02-29 01:43:45 +0900

    feat: 피보나치 수 solve

--- 피보나치 수.playground/Contents.swift
+++ 피보나치 수.playground/Contents.swift
@@ -1,3 +1,15 @@
-import UIKit
+func solution(_ n:Int) -> Int {
+    var F = [0, 1]
+    
+    for i in 2...n {
+        F.append((F[i - 2] + F[i - 1]).quotientAndRemainder(dividingBy: 1234567).remainder)
+    }
+    
+    return F.last!
+}
 
-var greeting = "Hello, playground"
+//    n    return
+//    3    2
+//    5    5
+
+print(solution(100000))
