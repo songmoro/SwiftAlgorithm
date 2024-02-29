36e54d6b04d2935cb61355dedc0648fd475e79a7
songmoro <wlqgkr@naver.com>
2024-02-29 18:55:41 +0900

    feat: 점프와 순간 이동 solve

---
+++ 점프와 순간 이동.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    n.nonzeroBitCount
+}
+
+// 101
+// 110
+// 1001110001000
+
+//    N    result
+//    5    2
+//    6    2
+//    5000    5
+
+print(solution(5000))
